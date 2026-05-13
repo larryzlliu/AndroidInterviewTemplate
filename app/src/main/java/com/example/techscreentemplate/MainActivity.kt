ce93b79e074d7f9ffb3230d0276978011ceafc2b
LARRY LIU <larryliu@ea:a6:75:51:79:fb.home>
2026-05-12 21:38:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/techscreentemplate/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.techscreentemplate
+
+import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.techscreentemplate.ui.theme.TechScreenTemplateTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TechScreenTemplateTheme {
+                Screen()
+            }
+        }
+    }
+}
+
+@HiltAndroidApp
+class MyApplication : Application()
